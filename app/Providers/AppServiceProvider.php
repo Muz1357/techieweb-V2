0081863ad094478ba4a32f65edcbca81acf29fe5
Muz1357 <muzeenmohammed6@gmail.com>
2025-09-03 15:32:46 +0530

    Commit 6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,12 +4,19 @@ namespace App\Providers;
 
 use App\Models\Product;
 use App\Observers\ProductObserver;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        // Force HTTPS in production so forms/assets always use https://
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        // Register product observer
         Product::observe(ProductObserver::class);
     }
 }
